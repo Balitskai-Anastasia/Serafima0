6ee6528b0a9278cb25aa8e5ecab7cb29315a221c
Balitskai-Anastasia <anastasiabalickaa435@gmail.com>
2023-03-01 10:52:38 +0200

    add new

---
+++ ConsoleApp46/ConsoleApp46/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp46
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { -5, 6, 1, 3, 0, -2 };
+            for (int i = 0; i < omas.Length - 1; i++)
+            {
+                for (int j = i + 1; j < omas.Length; j++)
+                    if (omas[i] > omas[j])
+                    {
+                        {
+                            int p = omas[i];
+                            omas[i] = omas[j];
+                            omas[j] = p;
+                        }
+                    }
+                foreach (int el in omas)
+                {
+                    Console.Write($"{el} \t");
+                }
+            }
+        }
+    } 
+
+} 
